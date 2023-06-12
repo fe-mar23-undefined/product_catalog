f085e0f0edab3cb7b58db67ece30f783b7c12c07
ctgdprzyb <126746748+ctgdprzyb@users.noreply.github.com>
2023-06-12 19:07:20 +0200

    finish most of positioning TODO: implement fonts, make the footer links grow apart on larger screens

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,16 +5,28 @@ export const Footer: React.FC = () => {
   return (
     <footer className="footer">
         <div className="footer__logo">NICE👌<br/>GADGETS</div>
-        <nav>
+        <nav className="">
           <ul className="footer__nav">
-            <li className="footer__nav__link">GitHub</li>
-            <li className="footer__nav__link">Contacts</li>
-            <li className="footer__nav__link">Rights</li>
+            <li className="footer__nav__li">
+              <a href="https://github.com/fe-mar23-undefined/product_catalog" className="footer__nav__link">
+                GitHub
+              </a>
+            </li>
+            <li className="footer__nav__li">
+              <a href="" className="footer__nav__link">
+                Contacts
+              </a>
+            </li>
+            <li className="footer__nav__li">
+              <a href="" className="footer__nav__link">
+                Rights
+              </a>
+            </li>
           </ul>
         </nav>
         <div className="footer__backtotop">
-          <a href="#top" className="footer__backtotop__text">Back to top</a>
-          <a href="#top" className="footer__backtotop__button">&#62;</a>
+          <a href="#index" className="footer__backtotop__text">Back to top</a>
+          <a href="#index" className="footer__backtotop__button">&#62;</a>
         </div>
     </footer>
   )
