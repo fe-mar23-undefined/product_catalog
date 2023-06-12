c76e8025f5126f203fc3ef5192d05d4e45ed1d67
ctgdprzyb <126746748+ctgdprzyb@users.noreply.github.com>
2023-06-12 15:50:31 +0200

    add classes and very basic css

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,17 +1,20 @@
+import "./Footer.scss";
+// #TODO: change scss into module.scss or whatevr
+
 export const Footer: React.FC = () => {
   return (
-    <footer>
-        <div>NICE👌 GADGETS</div>
+    <footer className="footer">
+        <div className="footer__logo">NICE👌<br/>GADGETS</div>
         <nav>
-          <ul>
-            <li>GitHub</li>
-            <li>Contacts</li>
-            <li>Rights</li>
+          <ul className="footer__nav">
+            <li className="footer__nav__link">GitHub</li>
+            <li className="footer__nav__link">Contacts</li>
+            <li className="footer__nav__link">Rights</li>
           </ul>
         </nav>
-        <div>
-          <a href="#top">Back to top</a>
-          <button>&#62;</button>
+        <div className="footer__backtotop">
+          <a href="#top" className="footer__backtotop__text">Back to top</a>
+          <button className="footer__backtotop__button">&#62;</button>
         </div>
     </footer>
   )
