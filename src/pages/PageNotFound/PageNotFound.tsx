66f901c7d07b3d1dcf33c0ce68774b461122ef5e
PufAnimalsDev <93289768+PufAnimalsDev@users.noreply.github.com>
2023-06-12 16:12:41 +0200

    [UI]: Add Not Found page

--- src/pages/PageNotFound/PageNotFound.tsx
+++ src/pages/PageNotFound/PageNotFound.tsx
@@ -1,3 +1,12 @@
-export const PageNotFound = () => (
-  <h1>Sorry page you try to access doesn't exists</h1>
-);
+import { Link } from 'react-router-dom';
+import './PageNotFound.scss'
+
+export const PageNotFound = () =>
+  <>
+    <div className='not_found_section'>
+      <p className='not_found_section__error_code'>404</p>
+      <h1 className='not_found_section__title'>page not found</h1>
+      <p className='not_found_section__message'>The Page you’re looking for was not found. Probably It’s Deleted, click the button below to go Home.</p>
+      <Link className='not_found_section__back_to_home' to={`/home`}>Go to home</Link>
+    </div>
+  </>
