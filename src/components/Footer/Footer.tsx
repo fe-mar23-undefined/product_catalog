dc478be97e8a027d7412d2c13f78a8147c91698a
ctgdprzyb <126746748+ctgdprzyb@users.noreply.github.com>
2023-06-12 15:03:45 +0200

    create basic html for footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,18 @@
+export const Footer: React.FC = () => {
+  return (
+    <footer>
+        <div>NICE👌 GADGETS</div>
+        <nav>
+          <ul>
+            <li>GitHub</li>
+            <li>Contacts</li>
+            <li>Rights</li>
+          </ul>
+        </nav>
+        <div>
+          <a href="#top">Back to top</a>
+          <button>&#62;</button>
+        </div>
+    </footer>
+  )
+}
