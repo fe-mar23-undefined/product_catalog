424e7badefc196556426717cf48f9403ed36beb5
Paweł Smoliński <ski1993@o2.pl>
2023-06-13 16:59:58 +0200

    styling phonespage

--- src/pages/PhonesPage/PhonesPage.tsx
+++ src/pages/PhonesPage/PhonesPage.tsx
@@ -3,6 +3,8 @@ import { useEffect, useMemo, useState } from "react"
 import { getPhones } from "../../api/phones";
 import { Phone } from "../../types/Phone";
 import './PhonesPage.scss';
+import arrowImg from '../../images/icons/arrow.png';
+import homeImg from '../../images/icons/home.png';
 
 export const PhonesPage = () => {
   const [phones, setPhones] = useState<Phone[]>([])
@@ -90,84 +92,98 @@ export const PhonesPage = () => {
 
   return (
     <>
-      <h1>Mobile phones</h1>
-      <p>{phones.length} models</p>
-      <p>sort and pagination</p>
-      <div className="pagination">
-        <div className="sort">
-          <p className="sort__text">Sort by</p>
-          <select 
-            className="sort__select"
-            id="sortSelector"
-            value={sortValue}
-            onChange={handleChangeSortValue}
-            >
-              <option value="Newest">Newest</option>
-              <option value="Alphabetically">Alphabetically</option>
-              <option value="Cheapest">Cheapest</option>
-            </select>
+      <div className='phones-page'>
+        <div className='phones-page__breadcrumbs breadcrumbs'>
+          <img 
+            src={homeImg} 
+            className="breadcrumbs__homeImg"
+            alt="img of home"
+           />
+          <img 
+            src={arrowImg} 
+            alt="arrow img"
+            className="breadcrumbs__arrowImg"
+          />
+          <p className="breadcrumbs__text">Phones</p>
         </div>
-        <div className="perPage">
-          <p className="perPage__text">Items on page</p>
-          <select 
-            className="perPage__select"
-            id="perPageSelector"
-            value={perPageValue}
-            onChange={handleChangePerPageValue}
-            >
-              <option value={`${phones.length}`}>All</option>
-              <option value="16">16</option>
-              <option value="8">8</option>
-              <option value="4">4</option>
-            </select>
-        </div>
-      </div>
-      {!hasError 
-        ? <div className="phones">
-            {visiblePhones.map(phone => <div className="card" key={phone.id}>{phone.name} {phone.fullPrice} {phone.year}</div>)}
+        <h1 className='phones-page__title heading--h1'>Mobile phones</h1>
+        <p className='phones-page__amount text'>{phones.length} models</p>
+        <div className="pagination">
+          <div className="pagination__sort">
+            <p className="pagination__sort-text">Sort by</p>
+            <select 
+              className="pagination__sort-select"
+              id="sortSelector"
+              value={sortValue}
+              onChange={handleChangeSortValue}
+              >
+                <option value="Newest">Newest</option>
+                <option value="Alphabetically">Alphabetically</option>
+                <option value="Cheapest">Cheapest</option>
+              </select>
           </div>
-        : <p>Error</p>
-      }
-      <ul>
-        <li className={classNames('pagination__item', {
-          disabled: currentPage === 1,
-        })}
-        >
-          <a 
-            className="pagination__link"
-            href='#/phones'
-            onClick={handlePrevPage}
-          >
-            prev
-          </a>
-        </li>
-        {pages(lastPage).map(page => (
+          <div className="pagination__perPage">
+            <p className="pagination__perPage-text">Items on page</p>
+            <select 
+              className="pagination__perPage-select"
+              id="perPageSelector"
+              value={perPageValue}
+              onChange={handleChangePerPageValue}
+              >
+                <option value={`${phones.length}`}>All</option>
+                <option value="16">16</option>
+                <option value="8">8</option>
+                <option value="4">4</option>
+              </select>
+          </div>
+        </div>
+        {!hasError 
+          ? <div className="phones">
+              {visiblePhones.map(phone => <div className="card" key={phone.id}>{phone.name} {phone.fullPrice} {phone.year}</div>)}
+            </div>
+          : <p>Error</p>
+        }
+        <ul>
           <li className={classNames('pagination__item', {
-          disabled: currentPage === 1,
-        })}
-        >
-          <a 
-            className="pagination__link"
-            href='#/phones'
-            onClick={() => handleChangePage(page)}
+            disabled: currentPage === 1,
+          })}
           >
-            {page}
-          </a>
-        </li>
-        ))}
-         <li className={classNames('pagination__item', {
-          disabled: currentPage === 1,
-        })}
-        >
-          <a 
-            className="pagination__link"
-            href='#/phones'
-            onClick={handleNextPage}
+            <a 
+              className="pagination__link"
+              href='#/phones'
+              onClick={handlePrevPage}
+            >
+              prev
+            </a>
+          </li>
+          {pages(lastPage).map(page => (
+            <li className={classNames('pagination__item', {
+            disabled: currentPage === 1,
+          })}
           >
-            next
-          </a>
-        </li>
-      </ul>
+            <a 
+              className="pagination__link"
+              href='#/phones'
+              onClick={() => handleChangePage(page)}
+            >
+              {page}
+            </a>
+          </li>
+          ))}
+          <li className={classNames('pagination__item', {
+            disabled: currentPage === 1,
+          })}
+          >
+            <a 
+              className="pagination__link"
+              href='#/phones'
+              onClick={handleNextPage}
+            >
+              next
+            </a>
+          </li>
+        </ul>
+      </div>
     </>
   )
 }
