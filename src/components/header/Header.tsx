9ceecccd49db4eab4da22a5578ae5b1f889078dd
marshalek37 <patrykmarp@gmai.com>
2023-06-13 11:08:57 +0200

    Adding styling

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,6 +4,7 @@ export const Header = () =>
 <div className="header">
 
   <div className='header__section'>
+  <div className="header__wrapper">
 
     <div className="header__element">
       NICE 👌
@@ -11,11 +12,19 @@ export const Header = () =>
       GADGETS
     </div>
 
+    <div className="header__element--2">HOME</div>
+    <div className="header__element--2">PHONES</div>
+    <div className="header__element--2">TABLETS</div>
+    <div className="header__element--2">ACCESSORIES</div>
+    </div>
+
   </div>
 
   <div className="header__section--1">
 
-    <div className="header__element--1">=</div>
+    <div className="header__element--3">+</div>
+    <div className="header__element--1">-</div>
+    <div className="header__list">=</div>
   </div>
 
 </div>
