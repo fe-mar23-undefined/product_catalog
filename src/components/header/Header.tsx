2e9230bd55d5c4558d50f3ac14b470262a0929fa
marshalek37 <patrykmarp@gmai.com>
2023-06-13 10:16:32 +0200

    2 commit

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,11 +4,18 @@ export const Header = () =>
 <div className="header">
 
   <div className='header__section'>
-    1
+
+    <div className="header__element">
+      NICE 👌
+      <br />
+      GADGETS
+    </div>
+
   </div>
 
   <div className="header__section--1">
-    <img src="" alt="" />
+
+    <div className="header__element--1">=</div>
   </div>
 
 </div>
