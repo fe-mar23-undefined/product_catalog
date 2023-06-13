6dcf4a963ce6ecea5649d37e42316975c7967b69
PufAnimalsDev <93289768+PufAnimalsDev@users.noreply.github.com>
2023-06-13 10:05:32 +0200

    add fonts and change naming convention

--- src/pages/PageNotFound/PageNotFound.tsx
+++ src/pages/PageNotFound/PageNotFound.tsx
@@ -3,10 +3,10 @@ import './PageNotFound.scss'
 
 export const PageNotFound = () =>
   <>
-    <div className='not_found_section'>
-      <p className='not_found_section__error_code'>404</p>
-      <h1 className='not_found_section__title'>page not found</h1>
-      <p className='not_found_section__message'>The Page you’re looking for was not found. Probably It’s Deleted, click the button below to go Home.</p>
-      <Link className='not_found_section__back_to_home' to={`/home`}>Go to home</Link>
+    <div className='not-found-section'>
+      <p className='not-found-section__error-code'>404</p>
+      <h1 className='not-found-section__title'>page not found</h1>
+      <p className='not-found-section__message'>The Page you’re looking for was not found. Probably It’s Deleted, click the button below to go Home.</p>
+      <Link className='not-found-section__back-to-home' to={`/home`}>Go to home</Link>
     </div>
   </>
