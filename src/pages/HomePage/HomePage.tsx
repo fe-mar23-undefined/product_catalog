9750ed0273ac6ffc275e211cd3f4063e6d9e7a94
marshalek37 <patrykmarp@gmai.com>
2023-06-16 11:51:47 +0200

    Added arrows and photos

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,7 +3,43 @@ import './HomePage.scss';
 
 export const HomePage = () => {
   const [currentImage, setCurrentImage] = useState(1);
+  const [phones, setPhones] = useState<Phone[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
+
+  const loadPhones = async () => {
+    try {
+      setIsLoading(true);
+      const loadedPhones = await getPhones();
+      setPhones(loadedPhones);
+    } catch (error) {
+      setHasError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const brandNewPhones = useMemo(() => {
+    let newPhones = [...phones];
+    return newPhones.sort((firstPhone, secondPhone) => secondPhone.year - firstPhone.year);
+  }, [phones]);
+
+  const hotPricesPhones = useMemo(() => {
+    let newPhones = [...phones];
+    return newPhones.sort((firstPhone, secondPhone) => firstPhone.price - secondPhone.price);
+  }, [phones]);
+
+  useEffect(() => {
+    loadPhones();
+  }, []);
+
   const totalImages = 3;
+
+  const images = [
+    'https://github.com/mate-academy/product_catalog/blob/main/public/img/banner-accessories.png?raw=true',
+    'https://github.com/mate-academy/product_catalog/blob/main/public/img/banner-phones.png?raw=true',
+    'https://github.com/mate-academy/product_catalog/blob/main/public/img/banner-tablets.png?raw=true',
+  ];
 
   const showNextImage = () => {
     const nextImage = currentImage < totalImages ? currentImage + 1 : 1;
@@ -21,19 +57,24 @@ export const HomePage = () => {
       <h1 className="title">Welcome to Nice Gadgets store!</h1>
 
       <section className="banner">
-        <div className="arrow arrow--left" onClick={showPreviousImage}></div>
+        <button className="arrow arrow--left" onClick={showPreviousImage} />
         <div className="content">
-          <div
-            className={`content__image content image--${currentImage}`}
-          ></div>
+          <img
+            className="content__image"
+            src={images[currentImage - 1]}
+            alt="carousel item"
+          />
         </div>
-        <div className="arrow arrow--right" onClick={showNextImage}></div>
+        <button className="arrow arrow--right" onClick={showNextImage} />
       </section>
+
       <div className="select__container">
         <div className="selector selector--1"></div>
         <div className="selector selector--2"></div>
         <div className="selector selector--3"></div>
       </div>
+
+      <Carousel phones={brandNewPhones} title="Brand new models" />
 
       <section className="section__category">
         <h2 className="section__title">Shop by category</h2>
@@ -75,7 +116,10 @@ export const HomePage = () => {
           </div>
         </div>
       </section>
+
+      <Carousel phones={hotPricesPhones} title="Hot prices" />
     </div>
     </>
   );
 };
+
